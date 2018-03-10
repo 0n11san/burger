4f3007a15702d79a0d1fc868b00090afd79e4072
0n11san <Jon@Jons-MacBook-Pro.local>
2018-03-09 22:57:36 -0500

    modifications

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,19 @@
-CREATE DATABASE burgers_db
-USE burgers_db
+-- 2. In the `db` folder, create a file named `schema.sql`. Write SQL queries this file that do the following:
+--
+--    * Create the `burgers_db`.
+--    * Switch to or use the `burgers_db`.
+--    * Create a `burgers` table with these fields:
+--      * **id**: an auto incrementing int that serves as the primary key.
+--      * **burger_name**: a string.
+--      * **devoured**: a boolean.
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INT (10) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR (250) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT '0',
+
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Happy Burger');
+INSERT INTO burgers (burger_name) VALUES ('Clown Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('T-REX Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mystery Burger');
